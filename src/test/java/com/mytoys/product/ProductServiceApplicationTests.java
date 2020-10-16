62e3ce49d036824d4396d04d8446cb4747c014c6
yaraarafa <arafa.yara@gmail.com>
2020-10-16 17:48:10 +0200

    Adding logs tointegration tests continued

--- src/test/java/com/mytoys/product/ProductServiceApplicationTests.java
+++ src/test/java/com/mytoys/product/ProductServiceApplicationTests.java
@@ -1,6 +1,7 @@
 package com.mytoys.product;
 
 import com.mytoys.product.controller.ProductController;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,12 +9,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Slf4j
 class ProductServiceApplicationTests {
     @Autowired
     private ProductController controller;
 
     @Test
     public void autowiring() throws Exception {
+        log.info("Asserting The Controller  is auto-wired with no problems");
         assertThat(controller).isNotNull();
     }
 
